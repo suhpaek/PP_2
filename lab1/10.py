2871d9898f1816003fba85d39d885a5a4378f5c4
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-23 04:15:21 +0500

    10

---
+++ lab1/10.py
@@ -0,0 +1,14 @@
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = "Hello"
+print(a)
