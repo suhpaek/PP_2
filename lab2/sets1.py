670b2beaa96e68126991f0982f70b86a5618c577
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 19:47:32 +0500

    sets

---
+++ lab2/sets1.py
@@ -0,0 +1,7 @@
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+set1 = {"abc", 34, True, 40, "male"}
