121b74f9f86f2e4eda6160dcbb25187362232aaa
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 17:55:38 +0500

    11

---
+++ lab2/bool.py
@@ -0,0 +1,16 @@
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+print(bool("Hello"))
+print(bool(15.7))
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
