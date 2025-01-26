586b18f89e72d028a520f4216dc399e57e052a79
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 19:53:43 +0500

    sets

---
+++ lab2/dict2.py
@@ -0,0 +1,22 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+
+x = thisdict.get("model")
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x)
