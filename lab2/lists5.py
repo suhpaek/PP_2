dae3d8afad30f2d7c34a6702804f7fab342ed7c4
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 18:34:36 +0500

    lists

---
+++ lab2/lists5.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
