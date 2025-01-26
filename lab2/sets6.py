69c965d5b6d55115dc862e7bbaf789c0bc4e79b7
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 19:51:35 +0500

    sets

---
+++ lab2/sets6.py
@@ -0,0 +1,19 @@
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1 | set2
+print(set3)
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1.union(set2, set3, set4)
+print(myset)
