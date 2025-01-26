aabeea54af3b4f1e3f4bae72a2ba7f62560e9786
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 18:35:50 +0500

    lists

---
+++ lab2/lists6.py
@@ -0,0 +1,12 @@
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+  thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
