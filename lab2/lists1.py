c77283e884a92678ae03c769cf8ae6a1957aaf87
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 18:30:20 +0500

    lists

---
+++ lab2/lists1.py
@@ -0,0 +1,9 @@
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
