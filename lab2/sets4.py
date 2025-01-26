66e6aaca795cbedf0ee8189e68f6f7b0127bd661
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 19:50:12 +0500

    sets

---
+++ lab2/sets4.py
@@ -0,0 +1,19 @@
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
