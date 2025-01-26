eedc74af59844e6cce666d4dc3c425a1d244dba5
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 18:39:32 +0500

    lists

---
+++ lab2/lists8.py
@@ -0,0 +1,11 @@
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
