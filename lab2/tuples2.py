b64800a1e5eaeb765e9ad48086bf537aafef589c
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 19:40:27 +0500

    tuples

---
+++ lab2/tuples2.py
@@ -0,0 +1,9 @@
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+  thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
