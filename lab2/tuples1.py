c73141c660e9c372c7cdc394c69f641f8a0bd91c
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 18:43:41 +0500

    tuples

---
+++ lab2/tuples1.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
