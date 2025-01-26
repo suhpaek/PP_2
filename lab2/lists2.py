7c1096f0b401f8bcce7bee33b268a2c148d1dfa4
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 18:31:34 +0500

    lists

---
+++ lab2/lists2.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
