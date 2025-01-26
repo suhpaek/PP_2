06e80b5d366996e091867ff3cd9116611d6ce612
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-01-26 19:49:13 +0500

    sets

---
+++ lab2/sets3.py
@@ -0,0 +1,19 @@
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
