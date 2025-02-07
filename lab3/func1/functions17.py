7a0a984e6fa7996c05f3dd461ff614eb888cd36c
Aruzhan Zhenissova <arukaszhen@gmail.com>
2025-02-07 05:30:05 +0500

    f

--- lab3/func1/functions17.py
+++ lab3/func1/functions17.py
@@ -1,9 +1,8 @@
-def has_33(nums):
-    for i in range(len(nums)-1):
-        if nums[i] == 3 and nums[i+1] == 3:
-            return True
-    return False
-    
-x = list(map(int, input().split()))
+def has_three_numbers(nums):
+    if len(nums) == 3:
+        return True
+    else:
+        return False
 
-print(has_33(x))
+x = list(map(int, input().split()))
+print(has_three_numbers(x))
--- lab3/func1/modules_for_14/threethreemodule.py
+++ lab3/func1/modules_for_14/threethreemodule.py
@@ -1,9 +1,8 @@
 def has_33(nums):
-    for i in range(len(nums)-1):
-        if nums[i] == 3 and nums[i+1] == 3:
-            return True
-    return False
-    
-x = list(map(int, input().split()))
+    if len(nums) == 3:
+        return True
+    else:
+        return False
 
+x = list(map(int, input().split()))
 print(has_33(x))
